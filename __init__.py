4225c5fd8f2cde2839bd8f3937d12dc9879adbef
OXOP <164915721+0XOPx@users.noreply.github.com>
2025-12-23 17:33:35 +0100

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -3,5 +3,5 @@ from .core import route, use, run_server
 from .templates.html import html_templates
 from .templates.tsx import tsx_templates
 
-__version__ = "1.0.0"
+__version__ = "1.0.2"
 __all__ = ["route", "use", "run_server", "html_templates", "tsx_templates"]
